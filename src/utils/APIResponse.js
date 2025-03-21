5464fbdaa87b1a45d0e81c063408fe03751fd081
Rahul Singh <85427061+rahulrely@users.noreply.github.com>
2025-03-21 22:33:12 +0530

    utils codes

---
+++ src/utils/APIResponse.js
@@ -0,0 +1,11 @@
+class APIResonse {
+    constructor(
+        statusCode,data,message = "Success"
+    ){
+        this.statusCode = statusCode,
+        this.message = message,
+        this.success = statusCode < 400
+    }
+}
+
+export {APIResonse};
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,20 @@
+// const name= (fn) => {async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 501).json({
+//             success:false,
+//             message :err.message
+//         })
+//     }
+// }}
+
+
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next (err))
+    }
+};
+
+
+export {asyncHandler};

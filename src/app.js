54269a688a4798e999f85dc75392969f40ca28a5
Rahul Singh <85427061+rahulrely@users.noreply.github.com>
2025-03-21 22:00:43 +0530

    no message

--- src/app.js
+++ src/app.js
@@ -1,148 +1,25 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const { google } = require('googleapis');
-const crypto = require('crypto');
-const express = require('express');
-const session = require('express-session');
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-/**
- * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.
- * To get these credentials for your application, visit
- * https://console.cloud.google.com/apis/credentials.
- */
-const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URL
-);
+const app = express();
 
-// Access scopes for YouTube API
-const scopes = [
-  'https://www.googleapis.com/auth/youtube.force-ssl'
-];
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
 
-/* Global variable that stores user credential in this code example.
- * ACTION ITEM for developers:
- *   Store user's refresh token in your data store if
- *   incorporating this code into your real app.
- *   For more information on handling refresh tokens,
- *   see https://github.com/googleapis/google-api-nodejs-client#handling-refresh-tokens
- */
-let userCredential = null;
+app.use(express.json({
+    limit:"20kb"
+}));
 
-async function main() {
-  const app = express();
+app.use(express.urlencoded({extended:true , limit :"16kb"}));
 
-  app.use(session({
-    secret: 'your_secure_secret_key', // Replace with a strong secret
-    resave: false,
-    saveUninitialized: false,
-  }));
-
-  // Example on redirecting user to Google's OAuth 2.0 server.
-  app.get('/', async (req, res) => {
-    // Generate a secure random state value.
-    const state = crypto.randomBytes(32).toString('hex');
-    // Store state in the session
-    req.session.state = state;
-
-    // Generate a url that asks permissions for the Drive activity and Google Calendar scope
-    const authorizationUrl = oauth2Client.generateAuthUrl({
-      // 'online' (default) or 'offline' (gets refresh_token)
-      access_type: 'offline',
-      /** Pass in the scopes array defined above.
-        * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
-      scope: scopes,
-      // Enable incremental authorization. Recommended as a best practice.
-      include_granted_scopes: true,
-      // Include the state parameter to reduce the risk of CSRF attacks.
-      state: state
-    });
-
-    res.redirect(authorizationUrl);
-  });
-
-  // Receive the callback from Google's OAuth 2.0 server.
-  app.get('/oauth2callback', async (req, res) => {
-    // Handle the OAuth 2.0 server response
-    let q = url.parse(req.url, true).query;
-
-    if (q.error) { // An error response e.g. error=access_denied
-      console.log('Error:' + q.error);
-    } else if (q.state !== req.session.state) { //check state value
-      console.log('State mismatch. Possible CSRF attack');
-      res.end('State mismatch. Possible CSRF attack');
-    } else { // Get access and refresh tokens (if access_type is offline)
-      let { tokens } = await oauth2Client.getToken(q.code);
-      oauth2Client.setCredentials(tokens);
-
-      /** Save credential to the global variable in case access token was refreshed.
-        * ACTION ITEM: In a production app, you likely want to save the refresh token
-        *              in a secure persistent database instead. */
-      userCredential = tokens;
-      
-      // Example of using YouTube API to list channels.
-      var service = google.youtube('v3');
-      service.channels.list({
-        auth: oauth2Client,
-        part: 'snippet,contentDetails,statistics',
-        forUsername: 'GoogleDevelopers'
-      }, function (err, response) {
-        if (err) {
-          console.log('The API returned an error: ' + err);
-          return;
-        }
-        var channels = response.data.items;
-        if (channels.length == 0) {
-          console.log('No channel found.');
-        } else {
-          console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
-            'it has %s views.',
-            channels[0].id,
-            channels[0].snippet.title,
-            channels[0].statistics.viewCount);
-        }
-      });
-    }
-  });
-
-  // Example on revoking a token
-  app.get('/revoke', async (req, res) => {
-    // Build the string for the POST request
-    let postData = "token=" + userCredential.access_token;
-
-    // Options for POST request to Google's OAuth 2.0 server to revoke a token
-    let postOptions = {
-      host: 'oauth2.googleapis.com',
-      port: '443',
-      path: '/revoke',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
-      }
-    };
-
-    // Set up the request
-    const postReq = https.request(postOptions, function (res) {
-      res.setEncoding('utf8');
-      res.on('data', d => {
-        console.log('Response: ' + d);
-      });
-    });
-
-    postReq.on('error', error => {
-      console.log(error)
-    });
-
-    // Post the request with data
-    postReq.write(postData);
-    postReq.end();
-  });
+app.use(express.static("public"));
 
 
-  const server = http.createServer(app);
-  server.listen(8080);
-}
-main().catch(console.error);
+app.use(cookieParser());
+
+
+
+export {app} ;
--- src/index.js
+++ src/index.js
@@ -7,4 +7,12 @@ dotenv.config({
   path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log("Server is RUNNING at PORT :",process.env.PORT)
+  })
+}).
+catch((err)=>{
+  console.log("MONGODB CONNECTION FAILED !!!",err);
+});

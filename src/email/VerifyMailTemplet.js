c91688b0052f9b790bdf9a9876e3d149a2f14972
Rahul Singh <85427061+rahulrely@users.noreply.github.com>
2025-03-29 23:35:09 +0530

    changed Verify Email Templet

--- src/email/VerifyMailTemplet.js
+++ src/email/VerifyMailTemplet.js
@@ -10,10 +10,11 @@ const generateEmailHTML = (name, verifyCode) => {
 <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; text-align: center;">
     <div style="max-width: 400px; margin: 20px auto; background: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
         <h1 style="color: #0F7173; margin-bottom: 10px;">Retube</h1>
-        <h2 style="color: #333; font-size: 18px;">Hi ${name}</h2>
+        <h2 style="color: #333; font-size: 18px;">Hi ${name},</h2>
         <h2 style="color: #333; font-size: 18px;">Your Verification Code</h2>
         <p style="font-size: 16px; color: #555;">Use the code below to verify your email address.</p>
         <div style="font-size: 24px; font-weight: bold; color: #0F7173; margin: 15px 0;">${verifyCode}</div>
+        <p style="font-size: 14px; color: #777;">This verification code is valid for <strong>1 hour</strong>. After that, you'll need to request a new code.</p>
         <p style="font-size: 14px; color: #777;">If you didn’t request this, you can ignore this email.</p>
     </div>
 </body>

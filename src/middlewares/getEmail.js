9118b5ba72d4136b81925be035fa81dd03329577
Rahul Singh <rahul274301@gmail.com>
2025-06-28 14:39:56 +0530

    fix : some email for glg auth

--- src/middlewares/getEmail.js
+++ src/middlewares/getEmail.js
@@ -1,28 +1,19 @@
 import { APIError } from "../utils/APIError.js";
 import { asyncHandler } from "../utils/asynchandler.js";
 import CryptoJS from 'crypto-js';
-import jwt from "jsonwebtoken";
-import { APIError } from "../utils/APIError.js";
 
 export const getEmail = asyncHandler(async (req, res, next) => {
-    // const secret = process.env.EXPRESS_SESSION_SECRET;
-    // const { email } = req.query;
+    const secret = process.env.EXPRESS_SESSION_SECRET;
+    const { email } = req.query;
     
-    // if (!email) {
-    //     throw new APIError(400, "Email ID is required");
-    // }
-    // //Decode the email (prevents encoding issues)
-    // const decodedEmail = decodeURIComponent(email);
-
-    // const bytes = CryptoJS.AES.decrypt(decodedEmail, secret);
-    // const decryptEmail = bytes.toString(CryptoJS.enc.Utf8);
-    
-    const tempToken = req.cookies?.tempToken;
-    if(!tempToken){
-        throw new APIError(404,"No temp cookie found for Google Auth.");
+    if (!email) {
+        throw new APIError(400, "Email ID is required");
     }
-    const { email } = jwt.verify(tempToken, process.env.TEMP_TOKEN_SECRET); 
-    const decryptEmail = email;
+    //Decode the email (prevents encoding issues)
+    const decodedEmail = decodeURIComponent(email);
+
+    const bytes = CryptoJS.AES.decrypt(decodedEmail, secret);
+    const decryptEmail = bytes.toString(CryptoJS.enc.Utf8);
 
     //Basic email format validation (optional)
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
